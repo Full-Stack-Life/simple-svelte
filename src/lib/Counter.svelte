19d8dc955f4ef5ac9e2d69279a4aa0710a53ac0c
Samuel da Silva <samuel.silva@1doc.com.br>
2022-10-28 21:21:52 -0300

    test

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { count } from '../store/store';
+
+  let viewCount
+
+  count.subscribe(value => {
+    viewCount = value
+  })
+
+  const increment = () => {
+    count.update(value => value + 1)
+  }
+
+</script>
+
+<button on:click={increment}>
+  My count is {viewCount}
+</button>
